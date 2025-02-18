02ffe02697e594e649dda91ae51f9b6a9dcbea54
alexlinux <alex13.bonifacio@gmail.com>
2025-02-19 00:09:47 +0100

    almost done

--- inc/so_long.h
+++ inc/so_long.h
@@ -1,6 +1,14 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+// typedef struct s_data
+// {
+// 	void		*mlx_ptr; // MLX pointer
+// 	void		*win_ptr; // MLX window pointer
+// 	void		*textures[5]; // MLX image pointers (on the stack)
+// 	t_map		*map; // Map pointer (contains map details - preferably kept on the stack)
+// }	t_data;
+
 void	ft_print_error(void);
 
 #endif
